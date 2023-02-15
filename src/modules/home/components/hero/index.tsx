2d8fecbb88c5f5d6051b5feeb1b77edd1a3b3e5f
Stream Protocol / StreamPay <105049869+stream-protocol@users.noreply.github.com>
2023-02-15 12:59:24 +0200

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,10 +6,10 @@ const Hero = () => {
     <div className="h-[90vh] w-full relative">
       <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
         <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
-          Build on Stream Protocol: A decentralized e-commerce and payment platform
+          StreamPay: A Decentralized eCommerce Platform
         </h1>
         <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
-        A Blockchain-As-A-Service Platform allows businesses to start accepting cryptocurrency payments. Our store is built on the Solana blockchain, which provides a secure checkout experience for customers. The solution is easy to use and can be set up in just a few clicks, making it a convenient option for businesses looking to start accepting crypto payments.
+        Stream Protocol is building the next-generation payment infrastructure with Solana blockchain technology, and will provide a platform for bridging the gap between online merchants and cryptocurrency users.
         </p>
         <UnderlineLink href="/store">Explore products</UnderlineLink>
       </div>
@@ -18,9 +18,9 @@ const Hero = () => {
         layout="fill"
         loading="eager"
         priority={true}
-        quality={95}
+        quality={90}
         objectFit="cover"
-        alt="Photo by @streampay https://unsplash.com/
+        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
         className="absolute inset-0"
         draggable="false"
       />
